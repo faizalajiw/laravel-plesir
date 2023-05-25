5e95d1d9ea9dac83a3c7343152d1d43d69e6e808
faizalajiw <faizalajiwibowo7@gmail.com>
2023-05-25 18:53:11 +0700

    perbaikan logic login controller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,20 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
-// use DB;
-// use Auth;
-// use Session;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Session;
-
-use Carbon\Carbon;
-use App\Models\User;
 use Illuminate\Http\Request;
 use Brian2694\Toastr\Facades\Toastr;
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Http;
-use App\Rules\MatchOldPassword;
 
 class LoginController extends Controller
 {
@@ -36,17 +25,17 @@ class LoginController extends Controller
     use AuthenticatesUsers;
 
     /**
-    * Where to redirect users after login.
-    *
-    * @var string
-    */
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
     protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
-    * Create a new controller instance.
-    *
-    * @return void
-    */
+     * Create a new controller instance.
+     *
+     * @return void
+     */
     public function __construct()
     {
         $this->middleware('guest')->except([
@@ -55,13 +44,15 @@ class LoginController extends Controller
             'unlock'
         ]);
     }
-    /** index page login */
+
     public function login()
     {
         return view('auth.login');
     }
 
-    /** login with databases */
+    /**
+     * Authenticate the user.
+     */
     public function authenticate(Request $request)
     {
         $credentials = $request->validate([
@@ -72,7 +63,7 @@ class LoginController extends Controller
         if (Auth::attempt($credentials)) {
             $user = Auth::user();
 
-            Session::put([
+            session([
                 'name' => $user->name,
                 'user_id' => $user->user_id,
                 'email' => $user->email,
@@ -82,26 +73,22 @@ class LoginController extends Controller
 
             Toastr::success('Login berhasil :)', 'Success');
             return redirect()->intended('home');
-        } else {
-            Toastr::error('Gagal login, USERNAME ATAU PASSWORD SALAH :)', 'Error');
-            return redirect('login');
         }
-    }
-    
-    /** logout */
-    public function logout( Request $request)
-    {
-        Auth::logout();
-        // forget login session
-        $request->session()->forget('name');
-        $request->session()->forget('user_id');
-        $request->session()->forget('email');
-        $request->session()->forget('role_name');
-        $request->session()->forget('avatar');
-        $request->session()->flush();
 
-        Toastr::success('Logout successfully :)','Success');
+        Toastr::error('Gagal login, USERNAME ATAU PASSWORD SALAH :)', 'Error');
         return redirect('login');
     }
 
+    /**
+     * Log the user out of the application.
+     */
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+
+        Toastr::success('Logout successfully :)', 'Success');
+        return redirect('login');
+    }
 }
