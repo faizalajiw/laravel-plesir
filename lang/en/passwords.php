e7615afe0868e242a9ad3508a3a0b5d0332b24d9
faizalajiw <faizalajiwibowo7@gmail.com>
2023-06-09 19:35:20 +0700

    fix password custom lang

--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -13,10 +13,10 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
+    'reset' => 'Password berhasil diubah.',
+    'sent' => 'Silahkan cek email anda untuk me-reset password.',
     'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'user' => "Email address tidak ditemukan!",
 
 ];
